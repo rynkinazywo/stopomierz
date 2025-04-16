f343c96c911f272dc33e97eb34ecc22c784d6bea
rynkinazywo <rynkinazywo@gmail.com>
2025-04-16 23:17:57 +0200

    Update fra_scraper.py

--- fra_scraper.py
+++ fra_scraper.py
@@ -16,6 +16,7 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.binary_location = '/usr/bin/chromium-browser'  # ← to dodajemy
 
 driver = webdriver.Chrome(options=options)
 
